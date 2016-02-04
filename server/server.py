216896c77fb3707c57cf5395687050a5ead39376
jgladch <jeffgladchun@gmail.com>
2016-02-03 16:23:56 -0800

    Cleanup

--- server/server.py
+++ server/server.py
@@ -1,10 +1,6 @@
 import zerorpc
 import dymax
 
-class HelloRPC(object):
-    def hello(self, name):
-        return "Hello, %s" % name
-
 s = zerorpc.Server(dymax)
 s.bind("tcp://0.0.0.0:4242")
 s.run()
